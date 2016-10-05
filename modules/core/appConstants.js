6f93220ff67730a9298adc053c3df92e045988db
Sinem Semsioglu <sinem93@gmail.com>
2016-10-05 15:39:05 +0300

    general constants and http request module added

---
+++ modules/core/appConstants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  geo: {
+    istanbulLat: "41.015137",
+    istanbulLng: "28.979530"
+  }
+}
---
+++ modules/core/http/index.js
@@ -0,0 +1,76 @@
+var https = require("https");
+
+function makeRequest (config) {
+  var req = https.request(config.options, (resp) => {
+    var status = resp.statusCode;
+
+    var data = "";
+    resp.setEncoding('utf8');
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    resp.on('end', () => {
+      if (config.successCallback) {
+        if (config.expectJSON) {
+          data = JSON.parse(data);
+        }
+
+        if (config.errorCodes && config.errorCodes.indexOf(status) >= 0) {
+          var errorMessage = constructErrorMessage(data, "problem with request",
+           config.errorMessageField);
+          onError(data, errorMessage, config.errorCallback);
+
+        } else if (config.successCallback) {
+          config.successCallback(data);
+        }
+      }
+    });
+  });
+
+  req.on('error', (e) => {
+    if (config.expectJSON) {
+      e = JSON.parse(e);
+    }
+
+    var errorMessage = constructErrorMessage(e, "problem with request",
+    config.errorMessageField)
+
+    onError(e, errorMessage, config.errorCallback);
+  });
+
+  // write data to request body
+  if (config.postData) {
+    req.write(config.postData);
+  }
+
+  req.end();
+};
+
+function constructErrorMessage(resp, defaultMessage, errorMessageField) {
+    var errorMessage = defaultMessage;
+
+    if (errorMessageField) {
+      var errorFieldComponents = errorMessageField.split(".");
+      var errorFieldDepth = errorFieldComponents.length;
+      var currLevel = resp;
+
+      for (var depth = 0; depth < errorFieldDepth; depth++) {
+        currLevel = currLevel[errorFieldComponents[depth]];
+      }
+
+      errorMessage += ", " + currLevel;
+    }
+
+    return errorMessage;
+}
+
+function onError (resp, message, errorCallback) {
+  if (errorCallback) {
+      errorCallback(resp);
+  }
+}
+
+module.exports = {
+  makeRequest: makeRequest
+}
